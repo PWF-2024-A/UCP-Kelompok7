08c09b1a02eeef78fb0af5a518e8148ac301ff2d
Dianitasya Masta <dianitasyanandamasta@gmail.com>
2024-05-03 11:26:29 +0700

    memperbaiki categories

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -15,14 +15,16 @@
                         </div>
                         <div>
                             @if (session('success'))
-                            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)" class="text-sm text-green-600 dark:text-green-400">
-                                {{ session('success')}}
-                            </p>
+                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                                    class="text-sm text-green-600 dark:text-green-400">
+                                    {{ session('success') }}
+                                </p>
                             @endif
                             @if (session('danger'))
-                            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)" class="text-sm text-red-600 dark:text-red-400">
-                                {{ session('danger') }}
-                            </p>
+                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                                    class="text-sm text-red-600 dark:text-red-400">
+                                    {{ session('danger') }}
+                                </p>
                             @endif
                         </div>
                     </div>
@@ -46,79 +48,80 @@
                                 </th>
                             </tr>
                         </thead>
-                    <tbody>
-                        @forelse ($todos as $todo)
-                        <tr class="odd:bg-white odd:dark:bg-gray-800 even:bg-gray-50 even:dark:bg-gray-700">
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                                <a href="{{ route('todo.edit', $todo) }}" class="hover:underline">{{ $todo->title }}</a>
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $todo->category->title }}
-                            </td>
-                            <td class="hidden px-6 py-4 md:block">
-                                @if ($todo->is_complete == false)
-                                <span
-                                    class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-                                        Ongoing
-                                </span>
-                                @else
-                                <span
-                                    class="bg-green-100 text-green-880 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
-                                        Completed
-                                </span>
-                                @endif
-                            </td>
-                            <td class="px-6 py-4">
-                                <div class="flex space-x-3">
-                                    {{-- Action Here --}}
-                                    @if ($todo->is_complete == false)
-                                    <form action="{{ route('todo.complete', $todo) }}" method="Post">
-                                        @csrf
-                                        @method('PATCH')
-                                        <button type="submit" class="text-green-600 dark:text-green-400">
-                                            Complete
-                                        </button>
-                                    </form>
-                                    @else
-                                    <form action="{{ route('todo.uncomplete', $todo) }}" method="Post">
-                                        @csrf
-                                        @method('PATCH')
-                                        <button type="submit" class="text-blue-600 dark:text-blue-400">
-                                            Uncomplete
-                                        </button>
-                                    </form>
-                                    @endif
-                                    <form action="{{ route('todo.destroy', $todo) }}" method="Post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-red-600 dark:text-red-400">
-                                            Delete
-                                        </button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                        @empty
-                        <tr class="bg-white dark:bg-gray-800">
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
-                                Empty
-                            </td>
-                        </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+                        <tbody>
+                            @forelse ($todos as $todo)
+                                <tr class="odd:bg-white odd:dark:bg-gray-800 even:bg-gray-50 even:dark:bg-gray-700">
+                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                        <a href="{{ route('todo.edit', $todo) }}"
+                                            class="hover:underline">{{ $todo->title }}</a>
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{ $todo->category ? $todo->category->title : 'No Category' }}
+                                    </td>
+                                    <td class="hidden px-6 py-4 md:block">
+                                        @if ($todo->is_complete == false)
+                                            <span
+                                                class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+                                                Ongoing
+                                            </span>
+                                        @else
+                                            <span
+                                                class="bg-green-100 text-green-880 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+                                                Completed
+                                            </span>
+                                        @endif
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <div class="flex space-x-3">
+                                            {{-- Action Here --}}
+                                            @if ($todo->is_complete == false)
+                                                <form action="{{ route('todo.complete', $todo) }}" method="Post">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <button type="submit" class="text-green-600 dark:text-green-400">
+                                                        Complete
+                                                    </button>
+                                                </form>
+                                            @else
+                                                <form action="{{ route('todo.uncomplete', $todo) }}" method="Post">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <button type="submit" class="text-blue-600 dark:text-blue-400">
+                                                        Uncomplete
+                                                    </button>
+                                                </form>
+                                            @endif
+                                            <form action="{{ route('todo.destroy', $todo) }}" method="Post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="text-red-600 dark:text-red-400">
+                                                    Delete
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr class="bg-white dark:bg-gray-800">
+                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                        Empty
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+                @if ($todosCompleted > 1)
+                    <div class="p-6 text-xl text-gray-900 dark:text-gray-100">
+                        <form action="{{ route('todo.deleteallcompleted') }}" method="Post">
+                            @csrf
+                            @method('DELETE')
+                            <x-primary-button>
+                                Delete All Completed Task
+                            </x-primary-button>
+                        </form>
+                    </div>
+                @endif
             </div>
-            @if ($todosCompleted > 1)
-            <div class="p-6 text-xl text-gray-900 dark:text-gray-100">
-                <form action="{{ route('todo.deleteallcompleted') }}" method="Post">
-                    @csrf
-                    @method('DELETE')
-                    <x-primary-button>
-                        Delete All Completed Task
-                    </x-primary-button>
-                </form>
-            </div>
-            @endif
         </div>
-    </div>
 </x-app-layout>
